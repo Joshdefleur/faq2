cc588d306c4eb2acbb5c25fb2eceb8936d763bf9
Joshdefleur <126767705+Joshdefleur@users.noreply.github.com>
2024-06-05 11:28:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+
+let div = document.getElementById('is');
+
+let display = 0;
+
+
+
+function hideShow(){
+
+    if(display == 1){
+        div.style.display = 'block';
+        display = 0;
+    }
+
+    else{
+        div.style.display = 'none';
+        display = 1;
+    }
+
+}
+
+
+
+
+let div2 = document.getElementById('isa')
+
+let display2 = 0;
+
+
+
+function hideShoww(){
+
+    if(display2 == 1){
+        div2.style.display = 'block';
+        display2 = 0;
+    }
+
+    else{
+        div2.style.display = 'none';
+        display2 = 1;
+    }
+
+}
+
+
+
+let div3 = document.getElementById('isb')
+
+let display3 = 0;
+
+
+
+function hideShowww(){
+
+    if(display3 == 1){
+        div3.style.display = 'block';
+        display3 = 0;
+    }
+
+    else{
+        div3.style.display = 'none';
+        display3 = 1;
+    }
+
+
+
+}
+
+
+
+
+let div4 = document.getElementById('isc')
+
+let display4 = 0;
+
+
+
+function hideShowwww(){
+
+    if(display4 == 1){
+        div4.style.display = 'block';
+        display4 = 0;
+    }
+
+    else{
+        div4.style.display = 'none';
+        display4 = 1;
+    }
+
+
+
+}
+
